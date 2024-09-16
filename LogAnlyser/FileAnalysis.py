cc671eda32ab4e158b8b30489b5adff05e54c40d
Shaik Saleem <107453070+fazishaik42@users.noreply.github.com>
2024-09-16 14:37:34 +0530

    Create FileAnalysis.py
    Analyses the File and Creates an execution point from where the actual reading of the file starts.

---
+++ LogAnlyser/FileAnalysis.py
@@ -0,0 +1,50 @@
+class FileAnalysis:
+    
+    def __init__(self) -> None:
+        
+        self._file_path__            =       "LogAnalyser/Model/DataLayer/x1.log"
+
+        self._output_path_           =       "LogAnalyser/Output/Analysis.log"
+
+        self._analysis_start         =       "End Display Current Environment"
+
+        self.file_start              =        False
+
+        
+    '''
+        --> Reading the lines.
+        --> Find the string of line to analyse and start the analysis
+    '''
+
+    def fileAnlyser(self):
+
+        with open(self._file_path__,"r")     as f:
+
+            _fileData_      =   f.readlines()
+            
+            counter         =   0
+
+            for i in _fileData_:
+
+                line_split  =   _fileData_[counter].split("|")
+
+                if self._analysis_start in line_split[0]:
+
+                    xcounter    =   counter
+
+                    self.fileAnlyseLines (_fileData_,xcounter)
+
+                counter+=1
+
+    '''
+        --> This Function will start the counter by skiping the Unnecessary lines.
+        --> Write all the Import Lines into the Analysis log.
+    '''
+
+    def fileAnlyseLines (self,_fileData_,xcounter):
+
+        for i in _fileData_[xcounter+1:]:
+
+            with open(self._output_path_ ,"a")  as out:
+
+                out.writelines(i)
