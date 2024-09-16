2038a50b9a82c9afd2390ec55e55f852260bfdf9
Shaik Saleem <107453070+fazishaik42@users.noreply.github.com>
2024-09-16 22:08:58 +0530

    Update Controller.py
    Added an Input paramter for the user to check the statements  based on his requirement.
    
    Input paramters can be 
    ERROR
    INFO
    WARN

--- LogAnlyser/Controller/Controller.py
+++ LogAnlyser/Controller/Controller.py
@@ -5,10 +5,13 @@ from FileFilter import FileFilter
 
 '''
     --> Intialising the Objects of the Two classes.
+    --> Adding an Input parameter for the User to select the Input parameter
 '''
 xs_Object       =   FileAnalysis()
 
-xs_file_filter  =   FileFilter()
+input_check     =   input("Enter the Input Parameter : ")
+
+xs_file_filter  =   FileFilter(input_check.upper())
 
 '''
     --> The File Analyser will break the file and makes the file more readable in python.
@@ -16,4 +19,8 @@ xs_file_filter  =   FileFilter()
 
 xs_Object.fileAnlyser()
 
+'''
+    --> The FileFilter class filters the statements based on the Input parameter.
+    --> After filtering the file, Seperates the respective Log statement in the LogLines file.
+'''
 xs_file_filter.applyingFilter()
