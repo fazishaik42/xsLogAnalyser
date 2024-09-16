fc7b447d262457d335545e1b5e0dcbc6ade24ae0
Shaik Saleem <107453070+fazishaik42@users.noreply.github.com>
2024-09-16 20:13:38 +0530

    Update Controller.py
    Created New Class for the File Filter.

--- LogAnlyser/Controller/Controller.py
+++ LogAnlyser/Controller/Controller.py
@@ -1,7 +1,19 @@
 import difflib
 import os
 from FileAnalysis import FileAnalysis
+from FileFilter import FileFilter
 
+'''
+    --> Intialising the Objects of the Two classes.
+'''
 xs_Object       =   FileAnalysis()
 
+xs_file_filter  =   FileFilter()
+
+'''
+    --> The File Analyser will break the file and makes the file more readable in python.
+'''
+
 xs_Object.fileAnlyser()
+
+xs_file_filter.applyingFilter()
