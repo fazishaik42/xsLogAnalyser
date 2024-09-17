3a5c9e94d7c3ee3450fce2bd35093f210aaafdf0
Shaik Saleem <107453070+fazishaik42@users.noreply.github.com>
2024-09-18 00:03:51 +0530

    Update FileAnalysis.py
    Added a test function to trigger the button.

--- LogAnlyser/Controller/FileAnalysis.py
+++ LogAnlyser/Controller/FileAnalysis.py
@@ -35,3 +35,10 @@ class FileAnalysis():
             with open(self._output_path_ ,"a")  as out:
                 out.writelines(i)
             test_counter+=1
+            
+    '''
+        --> this is just a test function to trigger the button.
+    '''
+
+    def buttonTest(self):
+        print("Button Worked")
