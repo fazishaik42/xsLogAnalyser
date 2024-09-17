c9bbda7e9b80161717a831e52971b9b2ffb0a478
Shaik Saleem <107453070+fazishaik42@users.noreply.github.com>
2024-09-18 00:05:13 +0530

    Update InputUI.py
            --> write the **kwargs for the below __init__ method.
            --> **kwrgs should accept the button text, Command function.

--- LogAnlyser/Controller/InputUI.py
+++ LogAnlyser/Controller/InputUI.py
@@ -1,10 +1,20 @@
-import tkinter
+from tkinter import *
+
+from FileAnalysis import FileAnalysis
 
 class InputUI:
-    
-    def __init__(self):
-        
-        self.xs_window  =   tkinter.Tk()
+
+    '''
+        --> write the **kwargs for the below __init__ method.
+        --> **kwrgs should accept the button text, Command function.
+    '''
+
+    def __init__(self,**kwargs):
+
+        self.xs_button_func     =   FileAnalysis()
+        self.xs_window          =   Tk()
+        self.xs_window_button   =   Button(text=kwargs.get("text"), command=kwargs.get("func"))
+        self.xs_window_button.pack()
 
     def xs_open_window(self):
 
