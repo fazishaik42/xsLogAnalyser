1eefd698b3ff2483ca8aa59f7f6878f50a25a822
Shaik Saleem <107453070+fazishaik42@users.noreply.github.com>
2024-09-18 00:02:18 +0530

    Update Controller.py
        --> Adding the UI Elements from this section.
        --> The below InputUI class is Initialised with a window.
        --> This class will create the starting window of the Project.
        --> Developer can write the Event trigger for the button.
        --> Import the class from which the button should trigger the function.
        --> Intialise the button with the event of the function.
        --> Now the InputUI object takes the **kwargs

--- LogAnlyser/Controller/Controller.py
+++ LogAnlyser/Controller/Controller.py
@@ -6,11 +6,11 @@ import tkinter
 from InputUI import InputUI
 
 
-
 '''
     --> Intialising the Objects of the Two classes.
     --> Adding an Input parameter for the User to select the Input parameter
 '''
+
 xs_Object       =   FileAnalysis()
 
 input_check     =   input("Enter the Input Parameter : ")
@@ -31,6 +31,20 @@ xs_Object.fileAnlyser()
 '''
 xs_file_filter.applyingFilter()
 
-xs_input_ui     =   InputUI()
+'''
+    --> Adding the UI Elements from this section.
+    --> The below InputUI class is Initialised with a window.
+    --> This class will create the starting window of the Project.
+    --> Developer can write the Event trigger for the button.
+    --> Import the class from which the button should trigger the function.
+    --> Intialise the button with the event of the function.
+    --> Now the InputUI object takes the **kwargs
+    /*
+        self.xs_button_func     =   FileAnalysis()
+        self.xs_window_button   =   Button(text="Start Analysis", command=self.xs_button_func.buttonTest)
+    */
+'''
+
+xs_input_ui     =   InputUI(text="Start LogAnalyser", func=xs_Object.buttonTest)
 
 xs_input_ui.xs_open_window()
