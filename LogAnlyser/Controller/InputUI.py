6ebd3f119c28d1347b5535f5992b0d5f50fa7b25
Shaik Saleem <107453070+fazishaik42@users.noreply.github.com>
2024-09-18 23:15:30 +0530

    Update InputUI.py
    Intialising the mainscreen

--- LogAnlyser/Controller/InputUI.py
+++ LogAnlyser/Controller/InputUI.py
@@ -1,7 +1,5 @@
 from tkinter import *
-
-from FileAnalysis import FileAnalysis
-
+import tkinter as tk
 class InputUI:
 
     '''
@@ -9,15 +7,27 @@ class InputUI:
         --> **kwrgs should accept the button text, Command function.
     '''
 
-    def __init__(self,**kwargs):
+    main_screen          =   tk.Tk()
 
-        self.xs_button_func     =   FileAnalysis()
+    def __init__(self):
+        
+        self.main_screen.geometry("600x400")
+        # self.xs_button_func     =   FileAnalysis()
+        # self.xs_window          =   Tk()
+        # self.xs_window_button   =   Button(text=kwargs.get("text"), command=kwargs.get("func"))
+        # self.xs_window_button.pack()
+
+    def xs_open_window(self,**kwargs):
         self.xs_window          =   Tk()
+        self.xs_window.title("LogAnalyser")
+        self.xs_window.minsize(width=500,height=500)
         self.xs_window_button   =   Button(text=kwargs.get("text"), command=kwargs.get("func"))
         self.xs_window_button.pack()
 
-    def xs_open_window(self):
-
-        self.xs_window.title("Input Parameter : ")
-        self.xs_window.minsize(width=500,height=500)
         self.xs_window.mainloop()
+
+    def xs_entry_func(self):
+        x1  =   self.xs_input
+        return x1
+    
+    main_screen.mainloop()
