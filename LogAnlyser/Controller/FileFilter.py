f6ffa5effec50476cf9d085188273c174ae4cf15
Shaik Saleem <107453070+fazishaik42@users.noreply.github.com>
2024-09-21 18:26:33 +0530

    Update FileFilter.py
    --> added kwargs to read input Seach string and Ignore String.
    --> If statement to Which will filter the log based on the Seach string and Ignore String.

--- LogAnlyser/Controller/FileFilter.py
+++ LogAnlyser/Controller/FileFilter.py
@@ -1,29 +1,36 @@
 import difflib
 import os
 from FileAnalysis import FileAnalysis
-# from InputUI import InputUI
 
 object_fileanalysis     =   FileAnalysis()
 
-# xs_ui_obj               =   InputUI()
-
 class FileFilter:
 
-    def __init__(self,input_check) -> None:
+    '''
+        --> added kwargs to read input Seach string and Ignore String.
+    '''
+
+    def __init__(self,**kwargs) -> None:
 
         self._output_path_      =   object_fileanalysis._output_path_
         self._filtered_path_    =   "LogAnalyser/Output/Filter.log"
         self._loglines_path_    =   "LogAnalyser/Output/LogLines.log"
-        self.input_check_p      =   "["+input_check+"]"
+        self.input_check_p      =   "["+kwargs.get("accept")+"]"
+        self.xs_ignore_search   =   "["+kwargs.get("ignore")+"]"
         self.counter            =   0  
-        
+    
+    '''
+        --> Method to extract the log statement based on the User criteria.
+        --> Method to ignore the log statements based on the User criteria.
+        --> If statement to Which will filter the log based on the Seach string and Ignore String.
+    '''
 
     def applyingFilter(self):
 
         with open(self._output_path_,"r")  as f:
             _fileData_      =   f.readlines()
             for lin in _fileData_:
-                if  self.input_check_p in lin:
+                if  (self.input_check_p in lin) or (self.xs_ignore_search not in lin):
                     file_array        = []
                     file_array.append(lin)
                     self.counter+=1
